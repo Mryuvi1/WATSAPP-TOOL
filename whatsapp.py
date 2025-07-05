54b6c4f35dceeee90c2af07661f649f695ddb35b
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-07-05 11:10:24 +0530

    Delete whatsapp.py

--- whatsapp.py
+++
@@ -1,8 +0,0 @@
-
-class WhatsApp:
-    def __init__(self, creds_path):
-        print(f"🟢 WhatsApp session loaded from {creds_path}")
-
-    def send_message(self, number, message):
-        print(f"📨 Sending '{message}' to {number} via creds.json...")
-        # Simulate success
